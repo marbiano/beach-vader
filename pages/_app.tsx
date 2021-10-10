8b638d6845a1172a77c57a57ffa4c15f0ec599a5
Martin Bavio <mbavio@gmail.com>
2021-10-10 20:34:06 -0300

    Add social media

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 // @ts-check
+import Head from 'next/head';
+
 import { AddressProvider } from '../hooks/use-address';
 import { globalCss } from '../stitches.config';
 
@@ -46,11 +48,34 @@ const globalStyles = globalCss({
   },
 });
 
+const meta = {
+  title: 'Beach Vader',
+  description: 'Darth Vader’s generated phrases as NFTs.',
+  image: 'https://vader.marbiano.com/og_image.png',
+};
+
 export default function MyApp({ Component, pageProps }) {
   globalStyles();
   return (
-    <AddressProvider>
-      <Component {...pageProps} />
-    </AddressProvider>
+    <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta content={meta.description} name="description" />
+        <meta property="og:url" content="https://vader.marbiano.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@marbiano3" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+      </Head>
+      <AddressProvider>
+        <Component {...pageProps} />
+      </AddressProvider>
+    </>
   );
 }
